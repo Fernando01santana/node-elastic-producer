3755a90b3a5c0fdefe5ea5cbe153155642ab0ed2
Fernando01santana <cool.fernando23@gmail.com>
2024-01-14 18:57:21 -0300

    fix:remove redis service in benefits service

--- src/modules/benefts/services/benefts.service.ts
+++ src/modules/benefts/services/benefts.service.ts
@@ -6,7 +6,6 @@ import {
 } from 'src/shared/common/axios/benefits.dto';
 import { HttpService } from 'src/shared/common/axios/httpService';
 import { ClientProxyApplication } from 'src/shared/common/rabbitmq/clientProxy/clientProxy';
-import { CacheReadService } from 'src/shared/common/redis/services/redis.service';
 import { benefitsQueueRequest } from '../dtos/benefts.dto';
 import BenefitsInterface from '../interfaces/benefts.interface';
 
@@ -15,7 +14,6 @@ export class BenefitsService implements BenefitsInterface {
   constructor(
     private readonly httpService: HttpService,
     private readonly rabbitmqService: ClientProxyApplication,
-    private readonly redisService: CacheReadService,
   ) {}
   async getBenefits(
     document: string,
@@ -31,15 +29,6 @@ export class BenefitsService implements BenefitsInterface {
   }
 
   async sendDocumntToQueue(documents: benefitsQueueRequest) {
-    //   documents.documents.forEach(async (element) => {
-    //     const keyCache = `customer-${element.substring(0, 3)}`;
-    //     await this.cacheWriteService.addInformationToCache(keyCache, element);
-    // });
-
-    // const documentExists = await this.redisService.readInformationFromCache(
-    //   `customer-${element.substring(0, 3)}`,
-    // );
-
     documents.documents.forEach(async (element) => {
       this.rabbitmqService
         .getClientProxy()
