cf7756adfcada276c538d6beff2cddda29316d98
Fernando01santana <cool.fernando23@gmail.com>
2024-01-14 19:38:56 -0300

    fix: create env example and adjust service

--- src/modules/benefts/services/benefts.service.ts
+++ src/modules/benefts/services/benefts.service.ts
@@ -6,6 +6,7 @@ import {
 } from 'src/shared/common/axios/benefits.dto';
 import { HttpService } from 'src/shared/common/axios/httpService';
 import { ClientProxyApplication } from 'src/shared/common/rabbitmq/clientProxy/clientProxy';
+import { CacheReadService } from 'src/shared/common/redis/services/redis.service';
 import { benefitsQueueRequest } from '../dtos/benefts.dto';
 import BenefitsInterface from '../interfaces/benefts.interface';
 
@@ -14,6 +15,7 @@ export class BenefitsService implements BenefitsInterface {
   constructor(
     private readonly httpService: HttpService,
     private readonly rabbitmqService: ClientProxyApplication,
+    private readonly redisService: CacheReadService,
   ) {}
   async getBenefits(
     document: string,
@@ -29,6 +31,11 @@ export class BenefitsService implements BenefitsInterface {
   }
 
   async sendDocumntToQueue(documents: benefitsQueueRequest) {
+    // documents.documents.forEach(async (element) => {
+    //   const keyCache = `customer-${element.substring(0, 3)}`;
+    //   await this.redisService.addInformationToCache(keyCache, element);
+    // });
+
     documents.documents.forEach(async (element) => {
       this.rabbitmqService
         .getClientProxy()
